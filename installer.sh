cacdcf072a5221d832acfae4d5bc444909a602a5
Bill Brock <bbrock@Bills-MacBook-Pro-2.local>
2015-04-02 20:52:48 -0400

    initial commit

---
+++ installer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+## vim
+rm ~/.vimrc
+rm ~/.vim
+ln -s ${BASEDIR}/.vimrc ~/.vimrc
+ln -s ${BASEDIR}/.vim/ ~/.vim
+
+rm ~/.zshrc
+## zsh
+ln -s ${BASEDIR}/.zshrc ~/.zshrc
+
+rm ~/.gitconfig
+## git
+ln -s ${BASEDIR}/gitconfig ~/.gitconfig
