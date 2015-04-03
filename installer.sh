fe7779e9bcdb43b414780a705d229c7298a3f0ec
Bill Brock <bbrock@spartansystems.co>
2015-04-02 21:03:15 -0400

    renamed files and udated script

--- installer.sh
+++ installer.sh
@@ -5,13 +5,15 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ## vim
 rm ~/.vimrc
 rm ~/.vim
-ln -s ${BASEDIR}/.vimrc ~/.vimrc
-ln -s ${BASEDIR}/.vim/ ~/.vim
+ln -s ${BASEDIR}/vimrc ~/.vimrc
+ln -s ${BASEDIR}/vim/ ~/.vim
 
 rm ~/.zshrc
+rm ~/.oh-my-zsh
 ## zsh
-ln -s ${BASEDIR}/.zshrc ~/.zshrc
+ln -s ${BASEDIR}/zshrc ~/.zshrc
+ln -s ${BASEDIR}/oh-my-zsh ~/.oh-my-zsh
 
 rm ~/.gitconfig
 ## git
-ln -s ${BASEDIR}/.gitconfig ~/.gitconfig
+ln -s ${BASEDIR}/gitconfig ~/.gitconfig
